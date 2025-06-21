1f70a444e10d991a0140e97c848f919baea189ec
kelvin2250 <phatthachdau123@gmail.com>
2025-06-21 15:12:29 +0700

    "m"

--- frontend/components/tabDetail/InvoiceTabs.jsx
+++ frontend/components/tabDetail/InvoiceTabs.jsx
@@ -27,7 +27,7 @@ export default function InvoiceTabs({ invoice, patient }) {
                 <>
                     <div className="mb-2 text-sm">
                         <p><strong>Ngày lập:</strong> {new Date(invoice.NgayLap).toLocaleDateString("vi-VN")}</p>
-                        <p><strong>Người lập:</strong> {invoice.NguoiLap}</p>
+                        <p><strong>Người lập:</strong>   {invoice.NguoiLap}</p>
                         <p><strong>Ghi chú:</strong> {invoice.GhiChu || "Không có"}</p>
                     </div>
                     <InvoiceDetail data={invoice} />
