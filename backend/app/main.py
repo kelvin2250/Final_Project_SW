9949a6ff517d6f466fee53c1c4471da6833dd3c6
kelvin2250 <phatthachdau123@gmail.com>
2025-06-21 13:45:37 +0700

    df

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # ✅ Đặt trước include_router
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # hoặc ["*"] nếu đang test
+    allow_origins=["http://localhost:5173", "http://localhost:5174"],  # hoặc ["*"] nếu đang test
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
